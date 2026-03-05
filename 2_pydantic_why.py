e766ee48cc275b713f64c59ed674053d712a3750
Baldeep <kambojbaldeep@gmail.com>
2026-03-05 16:56:02 +0530

    Optional

--- 2_pydantic_why.py
+++ 2_pydantic_why.py
@@ -1,13 +1,13 @@
 from pydantic import BaseModel
-from typing import List, Dict
+from typing import List, Dict, Optional
 
 class Patient(BaseModel):
 
     name: str
     age: int
     weight: float
-    married: bool
-    allergies: List[str]
+    married: Optional[bool] = False
+    allergies: Optional[List[str]] = None
     contact_details: Dict[str, str]
 
 
@@ -21,7 +21,7 @@ def insert_patient_data(patient: Patient):
     print(patient.contact_details)
     print("inserted into the database")
 
-patient_info = {"name": "Baldeep", "age": 30, "weight": 70.5, "married": True, "allergies": ["penicillin", "aspirin"], "contact_details": {"email": "baldeep@example.com", "phone": "123-456-7890"}}
+patient_info = {"name": "Baldeep", "age": 30, "weight": 70.5, "married": True, "contact_details": {"email": "baldeep@example.com", "phone": "123-456-7890"}}
 
 
 patient1 = Patient(**patient_info)
