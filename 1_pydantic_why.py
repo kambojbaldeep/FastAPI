9b2cadfb46dad1b03b3e73a728a81d24f13f8fa6
Baldeep <kambojbaldeep@gmail.com>
2026-03-05 14:47:16 +0530

    type enforcement with the if condition

--- 1_pydantic_why.py
+++ 1_pydantic_why.py
@@ -1,7 +1,10 @@
 def insert_patient_data(name: str, age: int):
 
-    print(name)
-    print(age)
-    print("inserted into the database")
+    if type(name) == str and type(age) ==  int:
+        print(name)
+        print(age)
+        print("inserted into the database")
+    else:
+        print("Invalid data types")
 
 insert_patient_data("Baldeep", "thirty")
