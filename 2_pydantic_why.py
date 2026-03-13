4d242a8941d78508638999cc7fa50e5be09fe9ca
Baldeep <kambojbaldeep@gmail.com>
2026-03-13 12:52:04 +0530

    Any url validation

--- 2_pydantic_why.py
+++ 2_pydantic_why.py
@@ -1,10 +1,11 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, AnyUrl
 from typing import List, Dict, Optional
 
 class Patient(BaseModel):
 
     name: str
     email: EmailStr
+    linkedin: AnyUrl
     age: int
     weight: float
     married: Optional[bool] = False
@@ -16,6 +17,7 @@ class Patient(BaseModel):
 def insert_patient_data(patient: Patient):
     print(patient.name)
     print(patient.email)
+    print(patient.linkedin)
     print(patient.age)
     print(patient.weight)
     print(patient.married)
@@ -23,7 +25,7 @@ def insert_patient_data(patient: Patient):
     print(patient.contact_details)
     print("inserted into the database")
 
-patient_info = {"name": "Baldeep","email": "abc@gmail.com", "age": 30, "weight": 70.5, "married": True, "contact_details": {"phone": "123-456-7890"}}
+patient_info = {"name": "Baldeep","email": "abc@gmail.com", "linkedin": "https://www.linkedin.com/in/baldeep-singh-kamboj/", "age": 30, "weight": 70.5, "married": True, "contact_details": {"phone": "123-456-7890"}}
 
 
 patient1 = Patient(**patient_info)
