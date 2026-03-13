58b2d417376e1ea692255f82da3af1ac7fca2e84
Baldeep <kambojbaldeep@gmail.com>
2026-03-13 16:11:50 +0530

    model_validator, mode = after

--- 4_model_validator.py
+++ 4_model_validator.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, model_validator
 from typing import List, Dict
 
 class Patient(BaseModel):
@@ -11,6 +11,13 @@ class Patient(BaseModel):
     allergies: List[str]
     contact_details: Dict[str, str]
 
+
+    @model_validator(mode="after")
+    def energency_contact(cls, model):
+        if model.age > 60 and "emergency" not in model.contact_details:
+            raise ValueError("Emergency contact is required for patients above 60")
+        return model
+
 def insert_patient_data(patient: Patient):
     print(patient.name)
     print(patient.email)
@@ -21,7 +28,7 @@ def insert_patient_data(patient: Patient):
     print(patient.contact_details)
     print("inserted into the database")
 
-patient_info = {"name": "Baldeep","email": "abc@hdfc.com", "age": 30, "weight": 70.5, "married": True, "contact_details": {"phone": "123-456-7890"}}
+patient_info = {"name": "Baldeep","email": "abc@hdfc.com", "age": 67,"allergies": ["peanuts", "shellfish"], "weight": 70.5, "married": True, "contact_details": {"phone": "123-456-7890", "emergency": "9876543210"}}
 
 
 patient1 = Patient(**patient_info)
