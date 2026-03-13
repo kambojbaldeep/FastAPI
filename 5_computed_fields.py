4994ae7681b66bc4b1cb8866d8c8ff005e3b9bc7
Baldeep <kambojbaldeep@gmail.com>
2026-03-13 16:19:55 +0530

    BMI calculation using computed_fields

--- 5_computed_fields.py
+++ 5_computed_fields.py
@@ -6,10 +6,17 @@ class Patient(BaseModel):
     name: str
     email: EmailStr
     age: int
-    weight: float
+    weight: float  #kg
+    height: float  #meters
     married: bool
     allergies: List[str]
     contact_details: Dict[str, str]
+
+    @computed_field
+    @property
+    def calculate_bmi(self) -> float:
+        bmi = round(self.weight / (self.height * self.height), 2)
+        return bmi
 
 
 def insert_patient_data(patient: Patient):
@@ -20,9 +27,10 @@ def insert_patient_data(patient: Patient):
     print(patient.married)
     print(patient.allergies)
     print(patient.contact_details)
+    print("BMI: ", patient.calculate_bmi)
     print("inserted into the database")
 
-patient_info = {"name": "Baldeep","email": "abc@hdfc.com", "age": 67,"allergies": ["peanuts", "shellfish"], "weight": 70.5, "married": True, "contact_details": {"phone": "123-456-7890", "emergency": "9876543210"}}
+patient_info = {"name": "Baldeep","email": "abc@hdfc.com", "age": 67,"allergies": ["peanuts", "shellfish"], "weight": 170.5, "height": 1.75, "married": True, "contact_details": {"phone": "123-456-7890", "emergency": "9876543210"}}
 
 
 patient1 = Patient(**patient_info)
