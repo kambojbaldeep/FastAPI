d83f6a5ea9717279f7c68ef8a6f84b3aa9839db7
Baldeep <kambojbaldeep@gmail.com>
2026-03-13 15:12:10 +0530

    a new file model_validator

---
+++ 4_model_validator.py
@@ -0,0 +1,29 @@
+from pydantic import BaseModel, EmailStr
+from typing import List, Dict
+
+class Patient(BaseModel):
+
+    name: str
+    email: EmailStr
+    age: int
+    weight: float
+    married: bool
+    allergies: List[str]
+    contact_details: Dict[str, str]
+
+def insert_patient_data(patient: Patient):
+    print(patient.name)
+    print(patient.email)
+    print(patient.age)
+    print(patient.weight)
+    print(patient.married)
+    print(patient.allergies)
+    print(patient.contact_details)
+    print("inserted into the database")
+
+patient_info = {"name": "Baldeep","email": "abc@hdfc.com", "age": 30, "weight": 70.5, "married": True, "contact_details": {"phone": "123-456-7890"}}
+
+
+patient1 = Patient(**patient_info)
+
+insert_patient_data(patient1)
